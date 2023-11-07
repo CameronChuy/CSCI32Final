2c3223743b1c0b22b2e0d274265b1a24a1dc2f58
Cameron <cameroncary00@gmail.com>
2023-11-06 19:24:08 -0800

    Fix newly introduced semantic error

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -24,7 +24,7 @@ const menuItems = [
 export default function Navbar() {
     return (
     <div className ="flex justify-end gap-2 m-4">
-        {menuItems.map((item, key) => (
+        {menuItems.map((item, index) => (
             <a key={index} href= {item.href} className="px-4 py-2 bg-gray-100 shadow" >{item.title}</a>
         ))}
         

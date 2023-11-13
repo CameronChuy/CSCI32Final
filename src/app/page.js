d6bedad2b626be71f36f24d1c16421448b7c0036
Cameron <cameroncary00@gmail.com>
2023-11-13 02:52:13 -0800

    Integrate supabase database and fix card generation.

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,31 @@ import Image from 'next/image'
 import Navbar from './components/Navbar'
 import PageTitle from './components/PageTitle'
 import PageContent from './components/PageContent'
+import Card from './components/Card'
+import { createClient } from '@supabase/supabase-js'
 
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://uctbeefthbopurqnxuvv.supabase.co', process.env.SUPABASE_SECRET)
 
-export default function Home() {
+export const revalidate = 0
+export default async function Home() {
+  
+const { data: Cards, error } = await supabase
+.from('Cards')
+.select("*")
+
+console.log(Cards)
+        
   return (
   <div>
     <Navbar />
     <PageTitle title="Home" />
     <PageContent />
+    <div className='flex  justify-between gap-4 w-full my-6 fle-wrap'>
+      {Cards.map((card, index) => (
+        <Card key={index} title={card.title} img={card.img} description={card.description} />
+      ))}
+    </div>
   </div>
   )
 }

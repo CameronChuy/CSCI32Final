af5775c6f4545fa24bca66ffdab722297fe9c07f
Cameron <cameroncary00@gmail.com>
2023-11-20 17:12:45 -0800

    Add Jewelry shopping gallery

--- src/app/components/PageTitle.js
+++ src/app/components/PageTitle.js
@@ -1,3 +1,3 @@
 export default function PageTitle({title}) {
-    return <h1 className="text-xl font-bold m-4 text-cyan-500">{title}</h1>
+    return <h1 className="text-xl font-bold m-4 text-orange-200">{title}</h1>
 }
--- src/app/jewelry/page.js
+++ src/app/jewelry/page.js
@@ -2,13 +2,28 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
 import PageContent from '../components/PageContent'
+import Card from '../components/Card'
+import { createClient } from '@supabase/supabase-js'
 
-export default function Contact() {
+const supabase = createClient('https://uctbeefthbopurqnxuvv.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
+
+export const revalidate = 0
+
+let { data: JewelryInfo, error } = await supabase
+  .from('Jewelry')
+  .select('*')
+
+export default async function Jewelry() {
   return ( 
     <div>
       <Navbar />
-      <PageTitle title="Contact" />
-      <PageContent content="This is how you would contact me:"/>
+      <PageTitle title="Jewelry" />
+      <PageContent content="My hand-crafted creations"/>
+      <div className='flex  justify-between gap-4 w-full my-6 fle-wrap'>
+        {JewelryInfo.map((card, index) => (
+          <Card key={index} title={card.title} img={card.img} description={card.description} />
+        ))}
+      </div>
     </div>
     )
 }
